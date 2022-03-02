80b9ce4131320289e14888b2257a05900e9f044a
amulya2505 <38892462+amulya2505@users.noreply.github.com>
2022-03-02 19:12:30 +0530

    Create response.js

---
+++ constants/response.js
@@ -0,0 +1,6 @@
+const successful = "API Hit successfully.";
+const badRequest = "Bad request";
+const invalidQuery = "Invalid query";
+const invalidParam = "Invalid params.";
+
+module.exports = { badRequest,successful, invalidQuery, invalidParam }
